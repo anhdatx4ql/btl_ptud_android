17067581b2f9bc29220c5d6c576eda6af0210cfd
toluen3000 <toluen3000@gmail.com>
2024-11-24 10:57:40 +0700

    thi cong chuc nang tu dong login va sua ten pro5

--- app/src/main/java/com/example/btl_ptud_android/activities/HomeLoginActivity.kt
+++ app/src/main/java/com/example/btl_ptud_android/activities/HomeLoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.btl_ptud_android.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +10,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.btl_ptud_android.R
 import com.example.btl_ptud_android.databinding.ActivityHomeLoginBinding
 import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.database
 
 class HomeLoginActivity : AppCompatActivity() {
@@ -25,6 +27,40 @@ class HomeLoginActivity : AppCompatActivity() {
         val myRef = database.getReference("message")
 
         myRef.setValue("Hello, Tôi là Đạt!")
+
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser != null) {
+            goToHomeScreen() // Người dùng đã đăng nhập
+        }
+
+    }
+
+    private fun goToHomeScreen() {
+        binding.prgbar.visibility = View.VISIBLE
+        val uid = FirebaseAuth.getInstance().currentUser?.uid
+
+        if (uid != null){
+            val db = Firebase.database
+            val usersRef = db.getReference("users").child(uid)
+
+            usersRef.get().addOnSuccessListener { dataSnapshot ->
+                if (dataSnapshot.exists()){
+                    val isAdmin = dataSnapshot.child("admin").getValue(Boolean::class.java)
+
+                    if (isAdmin == true){
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }else{
+                        val intent = Intent(this, HomeUserActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                }
+            }
+        }
+
+
     }
 
     private fun SignUpScreen() {
